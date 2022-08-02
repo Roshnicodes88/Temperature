99cdc5d4b675a7965b211fd5b522c3a8fba42cec
Roshnicodes88 <106426143+Roshnicodes88@users.noreply.github.com>
2022-08-02 17:42:10 +0530

    This changes the temperature .

---
+++ script.js
@@ -0,0 +1,28 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9/5) + 32);
+        return fahrenheit;
+    }
+
+    const fahToCel = (fehr) => {
+        let Celsius = Math.round((fehr - 32) * 5/9);
+        return Celsius;
+    }
+
+
+    let result;
+
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}&#176 Fahrenheit`;   
+    }else{
+        result = fahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}&#176 Celsius`;   
+    }
+}
